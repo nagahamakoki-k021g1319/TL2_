83e0530805b9603d01bf0c40384875fef8819adc
nagahama_kouki <k021g1319@g.neec.ac.jp>
2023-11-27 15:43:04 +0900

    02_08完了

--- Resourse/convert.ps1
+++ Resourse/convert.ps1
@@ -1,6 +1,11 @@
 $files = Get-Item *.png
 
-foreach($f in $files){
+$options = "-ml 1" 
+
+foreach($f in $files)
+{
     Start-Process -FilePath TextureConverter.exe -ArgumentList $f -Wait
+    Write-Output $f
+    Start-Process -FilePath TextureConverter.exe -ArgumentList $f,$options -Wait
 }
 pause
